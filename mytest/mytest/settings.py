b198e35374ebb7d0e690b095dd2e0f3fe86298da
Pitchayapa <overevereve@gmail.com>
2019-02-07 14:59:05 +0700

    num num num with template tag {{}}

--- mytest/mytest/settings.py
+++ mytest/mytest/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'mytest.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['test1/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- mytest/test1/views.py
+++ mytest/test1/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def show_mul (request,num):
-    response = str(num) *3
-    return render("%s" %response)
+    context = {'number' :num}
+    return render(request,'test1/mul.html',context)
