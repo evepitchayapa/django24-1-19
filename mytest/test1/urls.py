40cb55355e6405f4739aa6c177d36a712b21497f
Pitchayapa <overevereve@gmail.com>
2019-02-07 15:41:35 +0700

    input text not complet

--- mytest/test1/urls.py
+++ mytest/test1/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-
+app_name = 'test1'
 urlpatterns =[
-    path('<int:num>/',views.show_mul , ),
+    path('<int:num>/',views.show_mul ,name = 'input' ),
 ]
--- mytest/test1/views.py
+++ mytest/test1/views.py
@@ -7,3 +7,14 @@ def show_mul (request,num):
     context = {'number' :num,'num1':num*1,'num2':num*2,'num3':num*3,'num4':num*4,'num5':num*5,
                'num6':num*6,'num7':num*7,'num8':num*8,'num9':num*9}
     return render(request,'test1/mul.html',context)
+
+def input (request):
+    return render(request,'test1/input.html')
+
+def showinput (request):
+    num = request.POST['add_number']
+
+    context = {'number' :num,'num1':num*1,'num2':num*2,'num3':num*3,'num4':num*4,'num5':num*5,
+               'num6':num*6,'num7':num*7,'num8':num*8,'num9':num*9}
+
+    return render(request,'mul.html',context)
