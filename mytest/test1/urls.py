35161a178cb7682ef1645d38734b1225954e2fc2
Pitchayapa <overevereve@gmail.com>
2019-02-14 13:11:17 +0700

    use input text show multiplicate complete and use forloop in views and template html

--- mytest/test1/urls.py
+++ mytest/test1/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 app_name = 'test1'
 urlpatterns =[
-    path('<int:num>/',views.show_mul ,name = 'input' ),
+    path('<int:num>/',views.show_mul,name = 'showmul'  ),
+    path('',views.input,name = 'input'),
+    path('result/',views.showresult,name = 'showresult'),
 ]
--- mytest/test1/views.py
+++ mytest/test1/views.py
@@ -1,20 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 
 def show_mul (request,num):
 
+    lstval = []
+    for i in range(12):
+        lstval.append(str((i+1)*num))
 
-    context = {'number' :num,'num1':num*1,'num2':num*2,'num3':num*3,'num4':num*4,'num5':num*5,
-               'num6':num*6,'num7':num*7,'num8':num*8,'num9':num*9}
+    context = {'num':num,'number':lstval}
+
+    # context = {'number' :num,'num1':num*1,'num2':num*2,'num3':num*3,'num4':num*4,'num5':num*5,
+    #            'num6':num*6,'num7':num*7,'num8':num*8,'num9':num*9}
+
+    # context = {'num':num}
+    # for i in range(12):
+    #     context['number'+str(i+1)] = num*(i+1)
     return render(request,'test1/mul.html',context)
 
 def input (request):
     return render(request,'test1/input.html')
 
-def showinput (request):
+
+def showresult (request):
     num = request.POST['add_number']
+    nume = int(num)
 
-    context = {'number' :num,'num1':num*1,'num2':num*2,'num3':num*3,'num4':num*4,'num5':num*5,
-               'num6':num*6,'num7':num*7,'num8':num*8,'num9':num*9}
+    lstval = []
+    lstkey = [1]
+    for i in range(12):
+        num = int(request.POST['add_number'])
+        lstval.append((i+1)*num)
+    print(lstval)
+    context = {'num':num,'number':lstval}
 
-    return render(request,'mul.html',context)
+    return render(request,'test1/mul.html',context)
+
